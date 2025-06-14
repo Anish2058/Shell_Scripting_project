55f6de877e974666f2fb55edff2224a31c3edec0
Anish Timsina <anishtimsina899@gmail.com>
2025-06-14 07:59:24 +0545

    initial

---
+++ Shell_github_API_integration/list_pull_request.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+########################################################################
+#                                                                      #
+# AUTHOR : ANISH TIMSINA                                               #
+# DATE   : 14 Jun, 2025                                                #
+# VERSION: V1                                                          #
+#                                                                      #
+# Program to list pull requests for a given GitHub repository          #
+#                                                                      #
+########################################################################
+
+# GitHub API base URL
+API_URL="https://api.github.com"
+
+# GitHub credentials (must be set as environment variables or hardcoded - not recommended)
+USERNAME=$username
+TOKEN=$token
+
+# User and Repository information (passed as arguments)
+REPO_OWNER=$1
+REPO_NAME=$2
+
+#Function to make a GET request to GitHub API
+function github_api_get {
+	local endpoint="$1"
+	local url="${API_URL}/${endpoint}"
+
+	curl -s -u "${USERNAME}:${TOKEN}" "$url"
+}
+
+#Function to list pull request
+function list_pull_request {
+	local endpoint="repos/${REPO_OWNER}/${REPO_NAME}/pulls"
+
+	#fetch the list of pulls request to the repository
+	pull_request="$(github_api_get "$endpoint" | jq -r '.[] | "#\(.number): \(.title) [by \(.user.login)]"')" 
+
+	#Display the list
+	if [[ -z  "$pull_request" ]]; then
+		echo "No open pull request found for ${REPO_OWNER}/${REPO_NAME}."
+	else
+		echo "Pull Request for ${REPO_OWNER}/${REPO_NAME} : " 
+		echo "$pull_request"
+	fi
+}
+
+#Main Script
+echo "Fetching pull requests for ${REPO_OWNER}/${REPO_NAME}..."
+list_pull_request
